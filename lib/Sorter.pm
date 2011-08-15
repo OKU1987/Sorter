2346c38057e6c3d5414d8197292146ea0fe2918b
OKU_K <oku.87.s62.a4@gmail.com>
2011-08-16 02:30:26 +0900

    Edited lib/Sorter.pm via GitHub

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,61 @@
-# implement this
+#!/usr/bin/perl
+
+
+{ package Sorter;
+
+  sub new {
+      my $class = shift;
+      my $self = { Values => \() };
+      bless $self, $class;
+  }
+
+  sub set_values {
+      my $self = shift;
+      $self->{Values} = \@_;
+  }
+
+  sub get_values {
+      my $self = shift;
+      @{$self->{Values}};
+  }
+
+    
+  sub quick_sort {
+      my $class = shift;
+      my $count = shift;
+      my $left = shift; 
+      my $right = shift;
+      my @values = @_;
+      my $i = $left;
+      my $j = $right;
+      my $pivot = $values[($i+$j)/2];
+
+     
+      while ($i <= $j) {
+    while ($values[$i] < $pivot) { $i++; }
+	  while ($values[$j] > $pivot) { $j--; }
+
+	  if ($i > $j) { last; }
+	  
+	  my $temp = $values[$i];
+	  $values[$i] = $values[$j];
+	  $values[$j] = $temp;
+	  
+	  $i++;
+	  $j--;
+      }
+
+      if ($left + 1 < $right) {
+	  @values = Sorter->quick_sort($count+1, $left, $i-1, @values);
+	  @values = Sorter->quick_sort($count+1, $j+1, $right, @values);
+      }
+      @values
+  }
+    
+  sub sort {
+      my $self = shift;
+      my @values = $self->get_values;
+      $self->set_values( Sorter->quick_sort(0, 0, $#values, @values) );
+  }
+
+}
