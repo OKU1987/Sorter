a297f83796bcfe995c509cb7ff5bdf1d2f419d98
OKU_K <oku.87.s62.a4@gmail.com>
2011-08-16 13:24:38 +0900

    Edited lib/Sorter.pm via GitHub

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,60 +1,59 @@
 #!/usr/bin/perl
 
+package Sorter;
 
-{ package Sorter;
+sub new {
+    my $class = shift;
+    my $self = { Values => \() };
+    bless $self, $class;
+}
 
-  sub new {
-      my $class = shift;
-      my $self = { Values => \() };
-      bless $self, $class;
-  }
+sub set_values {
+    my $self = shift;
+    $self->{Values} = \@_;
+}
 
-  sub set_values {
-      my $self = shift;
-      $self->{Values} = \@_;
-  }
-
-  sub get_values {
-      my $self = shift;
-      @{$self->{Values}};
-  }
+sub get_values {
+    my $self = shift;
+    @{$self->{Values}};
+}
 
     
-  sub quick_sort {
-      my $class = shift;
-      my $left = shift; 
-      my $right = shift;
-      my @values = @_;
-      my $i = $left;
-      my $j = $right;
-      my $pivot = $values[($i+$j)/2];
+sub quick_sort {
+    my $class = shift;
+    my $left = shift; 
+    my $right = shift;
+    my @values = @_;
+    my $i = $left;
+    my $j = $right;
+    my $pivot = $values[($i+$j)/2];
 
      
-      while ($i <= $j) {
-    	while ($values[$i] < $pivot) { $i++; }
-	while ($values[$j] > $pivot) { $j--; }
+    while ($i <= $j) {
+      while ($values[$i] < $pivot) { $i++; }
+      while ($values[$j] > $pivot) { $j--; }
 
-	if ($i > $j) { last; }
+      if ($i > $j) { last; }
 	  
-	my $temp = $values[$i];
-	$values[$i] = $values[$j];
-	$values[$j] = $temp;
-	  
-	$i++;
-	$j--;
-      }
+      my $temp = $values[$i];
+      $values[$i] = $values[$j];
+      $values[$j] = $temp;
+  
+      $i++;
+      $j--;
+    }
 
-      if ($left + 1 < $right) {
-	  @values = Sorter->quick_sort($left, $i-1, @values);
-	  @values = Sorter->quick_sort($j+1, $right, @values);
-      }
-      @values
-  }
-    
-  sub sort {
-      my $self = shift;
-      my @values = $self->get_values;
-      $self->set_values( Sorter->quick_sort(0, $#values, @values) );
-  }
-
+    if ($left + 1 < $right) {
+      @values = Sorter->quick_sort($left, $i-1, @values);
+      @values = Sorter->quick_sort($j+1, $right, @values);
+    }
+    @values
 }
+    
+sub sort {
+    my $self = shift;
+    my @values = $self->get_values;
+    $self->set_values( Sorter->quick_sort(0, $#values, @values) );
+}
+
+l;
